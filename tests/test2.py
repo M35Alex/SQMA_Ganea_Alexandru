1f0fc4ed3096f98c02f7ef5659220f75b4be7bfd
Ganea Alex <64649656+M35Alex@users.noreply.github.com>
2024-12-15 15:51:50 +0200

    Add files via upload

--- tests/test2.py
+++ tests/test2.py
@@ -2,7 +2,7 @@ import unittest
 
 class TestCase2(unittest.TestCase):
     def test_example2(self):
-        print("test 1")
+        print("test 2")
         self.assertEqual(2 * 2, 4)
 
 if __name__ == "__main__":
